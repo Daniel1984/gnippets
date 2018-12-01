1dc0e52aa240722cdb789eee8cf22129cd27feab
daniel <aciddaniel@gmail.com>
2018-12-01 23:51:59 +0200

    3rd party wrapper with use of interfaces

---
+++ thirdPartyWrapper/main.go
@@ -0,0 +1,64 @@
+/*
+* The issue: you have 3rd party dependency (in this case logentries logger)
+* and you want it to output to logentries api when in production/staging or
+* development but output to standard output while dveeloping locally.
+*	One way of achieving this is to create a custom struct and interface
+*	where you specify most commonly used logentries methods or all of them if
+*	you'd like. newLogger method in this case check to seee if env variable was
+*	set and if connection was successful (prod/staging/development scenario) and
+*	returns logentries module instance. If any of those conditions fail it
+*	returns our custom logger that will output to standard output (local
+*	scenario)
+ */
+
+package main
+
+import (
+	"fmt"
+	"github.com/bsphere/le_go"
+	"os"
+)
+
+// we only use Println and Printf in our app but if needed all of
+// logentries methods could be replicated
+type logger interface {
+	Println(...interface{})
+	Printf(string, ...interface{})
+}
+
+// fmtLogger has now 2 methods to satisfy interface
+type fmtLogger struct{}
+
+func (l *fmtLogger) Println(args ...interface{}) {
+	fmt.Println(args...)
+}
+
+func (l *fmtLogger) Printf(s string, args ...interface{}) {
+	fmt.Printf(s, args...)
+}
+
+func main() {
+	logger := newLogger()
+	logger.Printf("Testing Printf %v", 123)
+}
+
+// both, our local fmtLogger and logentries instance, satisfy our logger interface
+// so we can conditionally return either of them from newLogger function
+func newLogger() logger {
+	leKey := os.Getenv("LOG_ENTRIES_KEY")
+	fmtFallback := &fmtLogger{}
+
+	if leKey == "" {
+		return fmtFallback
+	}
+
+	le, err := le_go.Connect(leKey)
+
+	if err != nil {
+		return fmtFallback
+	}
+
+	defer le.Close()
+
+	return le
+}
