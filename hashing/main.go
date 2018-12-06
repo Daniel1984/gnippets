c1da53b81f722a34fe5b996d905f15a9c5ffce24
daniel <aciddaniel@gmail.com>
2018-12-07 01:30:00 +0200

    making use of interface to reuse code + sha256 encryption example

--- hashing/main.go
+++ hashing/main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io"
 )
 
-func main() {
-	md5HashMsg := hashMd5("hello world")
-	fmt.Println(md5HashMsg)
+type hashInterface interface {
+	io.Writer
+	Sum(b []byte) []byte
 }
 
-func hashMd5(in string) string {
-	h := md5.New()
+func main() {
+	md5Hash := md5.New()
+	sha256Hash := sha256.New()
+
+	md5Msg := encryotMsg("hello world", md5Hash)
+	sha256Msg := encryotMsg("hello world", sha256Hash)
+
+	fmt.Println("md5 -", md5Msg)
+	fmt.Println("sha256 -", sha256Msg)
+}
+
+func encryotMsg(in string, h hashInterface) string {
 	h.Write([]byte(in))
 	hashedMsg := hex.EncodeToString(h.Sum(nil))
 	return hashedMsg
