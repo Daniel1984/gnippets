1b1c91a6db696260cd2272ab7a54848cd1d5b468
daniel <aciddaniel@gmail.com>
2018-12-08 00:02:41 +0200

    hmac crypto

--- hashing/main.go
+++ hashing/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/hmac"
 	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
@@ -17,14 +18,19 @@ func main() {
 	md5Hash := md5.New()
 	sha256Hash := sha256.New()
 
-	md5Msg := encryotMsg("hello world", md5Hash)
-	sha256Msg := encryotMsg("hello world", sha256Hash)
+	md5Msg := encryptMsg("hello world", md5Hash)
+	sha256Msg := encryptMsg("hello world", sha256Hash)
+
+	hmacKey := []byte("12a34")
+	hmacHash := hmac.New(sha256.New, hmacKey)
+	hmacMsg := encryptMsg("hello world", hmacHash)
 
 	fmt.Println("md5 -", md5Msg)
 	fmt.Println("sha256 -", sha256Msg)
+	fmt.Println("hmac -", hmacMsg)
 }
 
-func encryotMsg(in string, h hashInterface) string {
+func encryptMsg(in string, h hashInterface) string {
 	h.Write([]byte(in))
 	hashedMsg := hex.EncodeToString(h.Sum(nil))
 	return hashedMsg
