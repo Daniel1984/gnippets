c1980395967b28d1db955434f538062747f84044
daniel <aciddaniel@gmail.com>
2018-12-25 23:48:11 +0200

    composition over inheritance in GO

---
+++ inheritance/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+type author struct {
+	firstName string
+	lastName  string
+	bio       string
+}
+
+func (a author) fullName() string {
+	return fmt.Sprintf("%s %s", a.firstName, a.lastName)
+}
+
+type post struct {
+	title   string
+	content string
+	author
+}
+
+func (p post) details() {
+	fmt.Println("Title: ", p.title)
+	fmt.Println("Content: ", p.content)
+	fmt.Println("Author: ", p.fullName())
+	fmt.Println("Bio: ", p.bio)
+}
+
+func main() {
+	author1 := author{
+		"Foo",
+		"Bar",
+		"Golang Enthusiast",
+	}
+	post1 := post{
+		"Inheritance in Go",
+		"Go supports composition instead of inheritance",
+		author1,
+	}
+	post1.details()
+}
