9b7844a4a7cdfca6752d5afe41d2975b24a15e8f
daniel <aciddaniel@gmail.com>
2019-02-08 23:11:08 +0200

    setting default env variable

--- standard_library/get_set_env_variables/main.go
+++ standard_library/get_set_env_variables/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-func main() {
+func getEnvVariable() {
 	key := "DB_CONN"
 	connStr, ok := os.LookupEnv(key)
 
@@ -14,4 +14,25 @@ func main() {
 	}
 
 	log.Println(connStr)
+}
+
+func setUnsetEnvVariable() {
+	key := "DB_CONN"
+	defaultValue := "postgres:default"
+
+	if connStr, ok := os.LookupEnv(key); !ok {
+		log.Printf("The env variable %s is not set. Setting default value\n", key)
+		os.Setenv(key, defaultValue)
+	} else {
+		log.Printf("The env variable for %s key is %s\n", key, connStr)
+		return
+	}
+
+	connStr, _ := os.LookupEnv(key)
+	log.Printf("Got default variable set %s", connStr)
+}
+
+func main() {
+	getEnvVariable()
+	setUnsetEnvVariable()
 }
