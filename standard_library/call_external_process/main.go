36327cd2e0ce004bb7b3d2895efc716726b75069
daniel <aciddaniel@gmail.com>
2019-02-10 13:25:38 +0200

    2 options of running external process

--- standard_library/call_external_process/main.go
+++ standard_library/call_external_process/main.go
@@ -6,7 +6,7 @@ import (
 	"os/exec"
 )
 
-func main() {
+func v1() {
 	prc := exec.Command("ls", "-a")
 	out := bytes.NewBuffer([]byte{})
 	prc.Stdout = out
@@ -17,7 +17,30 @@ func main() {
 	}
 
 	if prc.ProcessState.Success() {
-		fmt.Println("'ls -a' rocess output:")
+		fmt.Println("'ls -a' rocess output v1:")
 		fmt.Println(out.String())
 	}
+}
+
+func v2() {
+	prc := exec.Command("ls", "-a")
+	out := bytes.NewBuffer([]byte{})
+	prc.Stdout = out
+
+	err := prc.Start()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	prc.Wait()
+
+	if prc.ProcessState.Success() {
+		fmt.Println("'ls -a' rocess output v2:")
+		fmt.Println(out.String())
+	}
+}
+
+func main() {
+	v1()
+	v2()
 }
